d6012b9ce6857ad44682aa0ffee3964bfa375d5d
ahashi1263 <ahmedhashi1263@gmail.com>
2025-01-03 22:38:40 -0500

    Changing other images and global color

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,28 +6,28 @@ export default function BeyondResume() {
     <div className="min-h-screen flex flex-col items-center p-8 sm:p-16 bg-white dark:bg-gray-900">
       <div className="columns-1 md:columns-4 lg:columns-3 gap-4">
         <Image
-            src="/godfather.webp"
+            src="godfather.webp"
             alt="some gif"
             width={470}
             height={250}
             className="mb-4 w-full"
         />
         <Image
-            src="/one-piece-op.gif"
+            src="one-piece-op.gif"
             alt="one piece opening"
             width={470}
             height={250}
             className="mb-4 w-full"
         />
         <Image
-            src="/lesamourai.gif"
+            src="lesamourai.gif"
             alt="cold gif"
             width={470}
             height={200}
             className="mb-4 w-full"
         />
         <Image
-            src="/lahaine.webp"
+            src="lahaine.webp"
             alt="la haine"
             width={470}
             height={200}
@@ -35,42 +35,42 @@ export default function BeyondResume() {
         />
         
         <Image
-            src="/cold.gif"
+            src="cold.gif"
             alt="cold gif"
             width={470}
             height={400}
             className="mb-4 w-full"
         />
         <Image
-            src="/peyton.webp"
+            src="peyton.webp"
             alt="manning"
             width={470}
             height={400}
             className="mb-4 w-full"
         />
         <Image
-            src="/camera.webp"
+            src="camera.webp"
             alt="camera guy"
             width={470}
             height={300}
             className="mb-4 w-full"
         />
         <Image
-            src="/champloo.gif"
+            src="champloo.gif"
             alt="champloo gif"
             width={480}
             height={260}
             className="mb-4 w-full"
         />
         <Image
-            src="/bron.webp"
+            src="bron.webp"
             alt="lebron gif"
             width={470}
             height={250}
             className="mb-4 w-full"
         />
         <Image
-            src="/zoro.gif"
+            src="zoro.gif"
             alt="zoro"
             width={470}
             height={250}
